f24f0d6742c3a84619360287bfbafab368d280da
LoveriusB <loverius.bruno@gmail.com>
2022-05-27 09:55:14 +0200

    refactor the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Grid } from "@material-ui/core";
 import "./Components/Css/App.css";
+import DraggableWindow from "./Components/Drag/DraggableWindow";
 import Terminal from "./Components/Terminal/Terminal";
 
 const App = () => {
   return (
     <Grid style={{ width: "100%", height: "100vh" }} className={"master"}>
-      <Terminal />
+      <DraggableWindow>
+        <Terminal />
+      </DraggableWindow>
     </Grid>
   );
 };
