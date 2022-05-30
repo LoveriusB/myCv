4b920f46a662ba95bdf26b9adfd06a1558d2bd67
LoveriusB <loverius.bruno@gmail.com>
2022-05-30 20:40:41 +0200

    bug with svg elements not fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Grid } from "@material-ui/core";
 import "./Components/Css/App.css";
-import DraggableWindow from "./Components/Drag/DraggableWindow";
-import Terminal from "./Components/Terminal/Terminal";
+import DraggableAndResizableWindow from "./Components/Windows/Drag/DraggableAndResizableWindow";
+import Terminal from "./Components/Windows/Terminal/Terminal";
 
 const App = () => {
   return (
-    <Grid style={{ width: "100%", height: "100vh" }} className={"master"}>
-      <DraggableWindow>
+    <Grid className={"master"}>
+      <DraggableAndResizableWindow>
         <Terminal />
-      </DraggableWindow>
+      </DraggableAndResizableWindow>
     </Grid>
   );
 };
